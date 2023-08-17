138fb162677f446d6695c2c5427c33ebefb86cb7
robertjohanson93 <robert.johanson.93@gmail.com>
2023-08-17 22:30:12 +0200

    Added wage structure
    Added a wage structure dashboard. Containing tabs, cards and a table with example player data

---
+++ src/lib/types.ts
@@ -0,0 +1,6 @@
+export interface Player {
+   player: string;
+   position: string;
+   playingTime: string;
+   salary: string;
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import type { Player } from './types';
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getYouthPlayers(data: Player[]) {
+  return data.filter((item) => {
+    return item.playingTime === 'Youngster' || item.playingTime === 'Breakthrough Prospect';
+  });
+}
+
+export function getFirstTeamPlayers(data: Player[]) {
+  return data.filter((item) => {
+    return item.playingTime !== 'Youngster' && item.playingTime !== 'Breakthrough Prospect';
+  });
 }
