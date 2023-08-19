81382a488d353d9a90e18249202bb6b7d51a0079
robertjohanson93 <robert.johanson.93@gmail.com>
2023-08-19 12:15:16 +0200

    Prettier --write

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,25 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 import type { Player } from './types';
- 
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function getYouthPlayers(data: Player[]) {
   return data.filter((item) => {
-    return item.playingTime === 'Youngster' || item.playingTime === 'Breakthrough Prospect';
+    return (
+      item.playingTime === 'Youngster' ||
+      item.playingTime === 'Breakthrough Prospect'
+    );
   });
 }
 
 export function getFirstTeamPlayers(data: Player[]) {
   return data.filter((item) => {
-    return item.playingTime !== 'Youngster' && item.playingTime !== 'Breakthrough Prospect';
+    return (
+      item.playingTime !== 'Youngster' &&
+      item.playingTime !== 'Breakthrough Prospect'
+    );
   });
 }
