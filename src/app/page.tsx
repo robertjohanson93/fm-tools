bba2248774c0372dde05ec5d786cbe9b22ffc684
Robert <robert.johanson.93@gmail.com>
2023-08-21 08:37:11 +0200

    fix: paragraph widths in mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ export default function Home() {
     <div className='flex min-h-screen flex-col items-start gap-6'>
       <section>
         <h1 className='scroll-m-20 lg:text-5xl relative'>FM Tools<Badge variant="default" className='absolute top-0 ml-2'>Beta</Badge></h1>
-        <p className='leading-7 [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 w-2/3'>
+        <p className='leading-7 [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 lg:w-2/3'>
           Welcome to FM Tools, gathering different tools you can use while
           playing Football Manager!
         </p>
-        <p className='leading-7 [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 w-2/3'>
+        <p className='leading-7 [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 lg:w-2/3'>
           This is a project started after trying out a lot of different ways to
           enhance a Football Manager save. After growing tired of the different
           options and long lists not optimized for your club, I wanted to build
--- src/app/wages/page.tsx
+++ src/app/wages/page.tsx
@@ -6,14 +6,14 @@ export default function Wages() {
     <div className='flex min-h-screen flex-col items-start gap-6'>
       <section>
         <h1 className='scroll-m-20 lg:text-5xl'>Wage Structure</h1>
-        <p className='leading-7 [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 w-2/3'>
+        <p className='leading-7 [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 lg:w-2/3'>
           Set up a wage structure for your team. Based on the whole club, the
           first team and the youth team you can divide salaries into differrent
           player roles. Hopefully this can help you in contract negotiations, to
           not overpay certain parts of your squad and keep every player happy
           with their wage and promised playing time.
         </p>
-        <p className='leading-7 [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6 w-2/3'>
+        <p className='leading-7 [&:not(:first-child)]:mt-6 [&:not(:last-child)]:mb-6lg:w-2/3'>
           This page is using on a 30-30-30-10 wage structure. Of your whole wage
           budget, 30% can be used for your key players, 30% for important
           players, 30% for backup/squad players, and 10% for youth prospects.
